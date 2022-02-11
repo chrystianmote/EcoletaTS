fe87b1c1e79af89acfd20878103602feb45aa76e
Chrystian Mote <chrystianmote@hotmail.com>
2022-02-11 20:15:52 -0300

    Instalando mysql2 para usar no MySQL 8.0

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -3,9 +3,11 @@
 import path from 'path';
 
 module.exports = {
-    client: 'mysql',
+    client: 'mysql2',
+    version: '8.0',
     connection: {
         host: '127.0.0.1',
+        port: 3306,
         user: 'root',
         password: '123456',
         database: 'dbteste'
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,9 +1,11 @@
 import knex from "knex";
 
 const connection = knex({
-    client: 'mysql',
+    client: 'mysql2',
+    version: '8.0',
     connection: {
         host: '127.0.0.1',
+        port: 3306,
         user: 'root',
         password: '123456',
         database: 'dbteste'
